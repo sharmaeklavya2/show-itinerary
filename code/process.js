bdd9b14e202cced9b482b9c5b5fb13138d2e2a74
Eklavya Sharma <ekurgn@gmail.com>
2026-04-16 07:25:45 +0530

    add compiled files

---
+++ code/process.js
@@ -0,0 +1,198 @@
+import airports from "airports.json" with { type: "json" };
+import places from "places.json" with { type: "json" };
+import countries from "countries.json" with { type: "json" };
+const iataToAirportInfo = new Map(airports.map(x => [x.iata, x]));
+const placeMap = new Map();
+function notUndef(x) {
+    return x !== undefined;
+}
+function fillPlaceMap() {
+    for (const placeInfo of places) {
+        if (placeInfo.city !== undefined) {
+            if (placeInfo.state !== undefined) {
+                placeMap.getOrInsert([placeInfo.city, placeInfo.state, placeInfo.country].join(', '), placeInfo);
+                placeMap.getOrInsert([placeInfo.city, placeInfo.state].join(', '), placeInfo);
+            }
+            placeMap.getOrInsert([placeInfo.city, placeInfo.country].join(', '), placeInfo);
+            placeMap.getOrInsert(placeInfo.city, placeInfo);
+        }
+        else if (placeInfo.state !== undefined) {
+            const stateInfo = { 'state': placeInfo.state, 'country': placeInfo.country, 'tz': placeInfo.tz };
+            placeMap.getOrInsert([placeInfo.state, placeInfo.country].join(', '), stateInfo);
+            placeMap.getOrInsert(placeInfo.state, stateInfo);
+        }
+    }
+    for (const country of countries) {
+        if (country.city !== undefined) {
+            placeMap.getOrInsert([country.city, country.country].join(', '), country);
+            placeMap.getOrInsert(country.city, country);
+        }
+        placeMap.getOrInsert(country.country, { 'country': country.country, 'tz': country.tz });
+    }
+}
+fillPlaceMap();
+function strToZdt(s, tz) {
+    const [date, time] = s.split(' ');
+    const [year, month, day] = date.split('-').map(Number);
+    const [hour, minute] = time.split(':').map(Number);
+    return Temporal.ZonedDateTime.from({ 'year': year, 'month': month, 'day': day, 'hour': hour, 'minute': minute, timeZone: tz }, { 'disambiguation': 'reject', 'overflow': 'reject' });
+}
+function durationToStr(d) {
+    d = d.round({ 'largestUnit': 'days', 'smallestUnit': 'minutes' });
+    const hh = String(d.hours).padStart(2, '0');
+    const mm = String(d.minutes).padStart(2, '0');
+    let dayStr = '';
+    if (d.days > 1) {
+        dayStr = String(d.days) + ' days, ';
+    }
+    else if (d.days === 1) {
+        dayStr = '1 day, ';
+    }
+    return `${dayStr}${hh}:${mm}`;
+}
+function timeToStr(t) {
+    const hh = String(t.hour).padStart(2, '0');
+    const mm = String(t.minute).padStart(2, '0');
+    return hh + ':' + mm;
+}
+function locTimeDt(locTime) {
+    const zdt = strToZdt(locTime.when, locTime.timezone);
+    locTime.date = zdt.toPlainDate().toLocaleString('en-GB', { day: "numeric", month: "short", year: "numeric", weekday: "short" });
+    locTime.time = timeToStr(zdt.toPlainTime());
+    locTime.stdTime = zdt.toString();
+    return zdt;
+}
+function computeDurations(trip) {
+    let prevEndZdt = undefined;
+    let prevTo = undefined;
+    const origTrip = Array.from(trip);
+    trip.length = 0;
+    for (const ride of origTrip) {
+        if (ride.from.timezone === undefined) {
+            throw new Error('timezone missing in ' + JSON.stringify(ride.from));
+        }
+        if (ride.to.timezone === undefined) {
+            throw new Error('timezone missing in ' + JSON.stringify(ride.to));
+        }
+        const zdt1 = locTimeDt(ride.from);
+        const zdt2 = locTimeDt(ride.to);
+        if (prevTo !== undefined && ride.type !== 'layover') {
+            const layover = { 'type': 'layover', 'from': prevTo, 'to': ride.from };
+            const d = zdt1.since(prevEndZdt);
+            const durStr = durationToStr(d);
+            layover.duration = durStr;
+            trip.push(layover);
+        }
+        const d = zdt2.since(zdt1);
+        const durStr = durationToStr(d);
+        ride.duration = durStr;
+        trip.push(ride);
+        if (ride.type === 'layover') {
+            prevEndZdt = undefined;
+            prevTo = undefined;
+        }
+        else {
+            prevEndZdt = zdt2;
+            prevTo = ride.to;
+        }
+    }
+}
+function processLoc(locTime) {
+    /* possibilities:
+     * air travel: airport code decides tz
+     * travel to/from major city: city decides tz
+     * travel to/from non-major city: state decides tz
+     * travel to/from non-city area (e.g., locality): city/state must be specified, and that decides tz
+     */
+    const origLocTime = JSON.stringify(locTime);
+    let airportInfo;
+    if (locTime.airport !== undefined) {
+        airportInfo = iataToAirportInfo.get(locTime.airport);
+    }
+    else if (locTime.where !== undefined) {
+        airportInfo = iataToAirportInfo.get(locTime.where);
+        if (airportInfo !== undefined) {
+            locTime.airport = locTime.where;
+        }
+    }
+    if (airportInfo !== undefined) {
+        if (locTime.city === undefined) {
+            locTime.city = airportInfo.city;
+        }
+        else if (locTime.city !== airportInfo.city) {
+            throw new Error(`city is ${locTime.city}, but airport ${locTime.airport} has city ${airportInfo.city}.`);
+        }
+    }
+    let placeInfo;
+    const parts = [locTime.city, locTime.state, locTime.country].filter(notUndef);
+    for (let i = 0; i < parts.length; ++i) {
+        const key = parts.slice(i, parts.length).join(', ');
+        placeInfo = placeMap.get(key);
+    }
+    if (placeInfo === undefined && locTime.where !== undefined) {
+        placeInfo = placeMap.get(locTime.where);
+    }
+    if (placeInfo !== undefined) {
+        if (placeInfo.city !== undefined && locTime.city === undefined) {
+            locTime.city = placeInfo.city;
+        }
+        if (placeInfo.state !== undefined && locTime.state === undefined) {
+            locTime.state = placeInfo.state;
+        }
+        if (locTime.country === undefined) {
+            locTime.country = placeInfo.country;
+        }
+    }
+    // Set TZ
+    if (locTime.timezone === undefined) {
+        if (airportInfo !== undefined) {
+            locTime.timezone = airportInfo.tz;
+        }
+        else if (placeInfo !== undefined) {
+            locTime.timezone = placeInfo.tz;
+        }
+        else {
+            throw new Error(`Could not determine timezone for ${origLocTime}.`);
+        }
+    }
+    // set loctype
+    if (airportInfo !== undefined) {
+        locTime.locType = 'airport';
+    }
+    else if (placeInfo !== undefined) {
+        if (placeInfo.city !== undefined) {
+            locTime.locType = 'city';
+        }
+        else if (placeInfo.state !== undefined) {
+            locTime.locType = 'state';
+        }
+        else {
+            locTime.locType = 'country';
+        }
+    }
+    else {
+        throw new Error(`No place label for ${origLocTime}.`);
+    }
+}
+function processLocsInTrip(trip) {
+    for (const ride of trip) {
+        processLoc(ride.from);
+        processLoc(ride.to);
+    }
+}
+function getTrackingUrls(trip) {
+    for (const ride of trip) {
+        if (ride.type === 'flight' && ride.carrier !== undefined && ride.number !== undefined) {
+            const [date, _] = ride.from.when.split(' ');
+            const [year, month, day] = date.split('-');
+            ride.trackUrl = ('https://www.flightstats.com/v2/flight-tracker/'
+                + `${ride.carrier}/${ride.number}?year=${year}&month=${month}&date=${day}`);
+        }
+    }
+}
+export default function processTrip(trip) {
+    processLocsInTrip(trip);
+    getTrackingUrls(trip);
+    computeDurations(trip);
+}
+//# sourceMappingURL=process.js.map
---
+++ code/render.js
@@ -0,0 +1,114 @@
+function getElem(tagName, options) {
+    const elem = document.createElement(tagName);
+    if (options !== undefined) {
+        if (options.text !== undefined) {
+            elem.textContent = options.text;
+        }
+        if (options.attrs !== undefined) {
+            for (const [k, v] of Object.entries(options.attrs)) {
+                elem.setAttribute(k, v);
+            }
+        }
+        if (options.classes !== undefined) {
+            for (const c of options.classes) {
+                elem.classList.add(c);
+            }
+        }
+        if (options.parent !== undefined) {
+            options.parent.appendChild(elem);
+        }
+    }
+    return elem;
+}
+function renderLocTime(locTime, parent) {
+    const primary = locTime.where ?? locTime.airport ?? locTime.city;
+    if (primary === undefined) {
+        throw new Error('Missing place label.');
+    }
+    let secParts;
+    if (primary === locTime.airport) {
+        // secParts = [locTime.point, locTime.city, locTime.state, locTime.country].filter(notUndef);
+        secParts = [locTime.point, locTime.city];
+    }
+    else {
+        secParts = [locTime.point];
+        if (locTime.locType === 'city') {
+            if (primary !== locTime.city) {
+                secParts.push(locTime.city);
+            }
+        }
+        else if (locTime.locType === 'state') {
+            secParts.push(locTime.city);
+            secParts.push(locTime.state);
+        }
+        else if (locTime.locType === 'country') {
+            secParts.push(locTime.city);
+            secParts.push(locTime.state);
+            secParts.push(locTime.country);
+        }
+    }
+    const secParts2 = secParts.filter(x => x !== undefined);
+    const secondary = (secParts2.length > 0) ? secParts2.join(', ') : undefined;
+    const locElem = getElem('div', { parent: parent, classes: ['loctime'] });
+    getElem('div', { parent: locElem, text: primary, classes: ['loctime-where'] });
+    if (secondary !== undefined) {
+        getElem('div', { parent: locElem, text: secondary, classes: ['loctime-point'] });
+    }
+    getElem('div', { parent: locElem, text: locTime.date, classes: ['loctime-date'] });
+    const timeElem = getElem('time', { parent: locElem, text: locTime.time, classes: ['loctime-time'] });
+    if (locTime.stdTime !== undefined) {
+        timeElem.setAttribute('datetime', locTime.stdTime);
+        timeElem.setAttribute('title', locTime.stdTime);
+    }
+    return locElem;
+}
+const STD_RIDE_TYPES = new Set(['flight', 'bus', 'train', 'metro', 'car', 'taxi']);
+export default function renderTrip(trip) {
+    const tripElem = getElem('div', { classes: ['trip'] });
+    for (const ride of trip) {
+        const isLayover = ride.type === 'layover';
+        const rideClasses = isLayover ? ['layover'] : ['ride'];
+        const rideElem = getElem('div', { parent: tripElem, classes: rideClasses });
+        if (!isLayover) {
+            const header = getElem('div', { parent: rideElem, classes: ['ride-head'] });
+            const headLeft = getElem('div', { parent: header, classes: ['ride-head-left'] });
+            const headRight = getElem('div', { parent: header, classes: ['ride-head-right'] });
+            if (ride.type === undefined) {
+                getElem('div', { parent: headLeft, classes: ['ride-type'] });
+            }
+            else if (STD_RIDE_TYPES.has(ride.type)) {
+                getElem('div', { parent: headLeft, classes: ['ride-type', 'ride-type-' + ride.type] });
+            }
+            else {
+                getElem('div', { parent: headLeft, text: ride.type, classes: ['ride-type'] });
+            }
+            if (ride.carrier !== undefined) {
+                getElem('div', { parent: headLeft, text: ride.carrier, classes: ['ride-carrier'] });
+            }
+            if (ride.number !== undefined) {
+                getElem('div', { parent: headLeft, text: ride.number, classes: ['ride-number'] });
+            }
+            if (ride.trackUrl !== undefined) {
+                getElem('a', { parent: headLeft, text: '[track]', classes: ['ride-track-url'],
+                    'attrs': { 'href': ride.trackUrl, 'target': '_blank', 'rel': 'noopener noreferrer' } });
+            }
+            if (ride.bookingInfo !== undefined) {
+                for (const [_, v] of Object.entries(ride.bookingInfo)) {
+                    getElem('div', { parent: headRight, text: v });
+                }
+            }
+        }
+        const body = getElem('div', { parent: rideElem, classes: ['ride-body'] });
+        if (!isLayover) {
+            const fromElem = renderLocTime(ride.from, body);
+            fromElem.classList.add('ride-from');
+        }
+        const durElem = getElem('div', { parent: body, text: ride.duration, classes: ['ride-dur'] });
+        if (!isLayover) {
+            const toElem = renderLocTime(ride.to, body);
+            toElem.classList.add('ride-to');
+        }
+    }
+    return tripElem;
+}
+//# sourceMappingURL=render.js.map
